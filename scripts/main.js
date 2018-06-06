d8143652561b5f85e430f3018092f9fb1c392c08
Sanket <sanket@fete.la>
2018-06-06 11:58:05 -0400

    This works

---
+++ scripts/main.js
@@ -0,0 +1,115 @@
+console.log('this is javascript');
+
+// this is the url for the autocomplete place api, to be used in conjunction with the places library, linked to in the html
+// var GooglePlaceUrl = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyD4LMyFRC81mK6dnBXiyeynSEdN3TREPTQ';
+const key = 'AIzaSyAtG1TS4IQZwExreX22pNhrI5MApTtNTaA';
+const visionURL = 'https://vision.googleapis.com/v1/images:annotate?key='+ key;
+const languageURL = 'https://language.googleapis.com/v1/documents:analyzeSentiment?key=' + key;
+
+var $input = $('[data-searchbox]')[0];
+var searchBox = new google.maps.places.SearchBox($input);
+
+var places;
+var arrPhotos;
+var dimPhoto;
+var arrPhotosURLs;
+var arrLabels;
+var bubbleArray;
+var histogram;
+searchBox.addListener('places_changed', function() {
+    places = searchBox.getPlaces();
+    console.log(places);
+    arrPhotos = places[0]["photos"]
+    dimPhoto = {
+        'maxHeight' : 300,
+        'maxWidth' : 300
+    }
+    arrPhotosURLs = arrPhotos.map(photo=>photo.getUrl(dimPhoto));
+    var arrPhotoPromises = arrPhotosURLs.map(url => {
+        $('[data-placecontent]').append($('<img>',{
+            'src': url}))
+
+        var visionRequest = {
+            "requests": [
+            {
+                "image": {
+                "source": {
+                    "imageUri": url,
+                }
+                },
+                "features": [
+                {
+                    "type": "LABEL_DETECTION",
+                }
+                ]
+            }
+            ]
+        }
+        // bubbleArray = [];
+        histogram = {};
+        return $.ajax({
+            url: visionURL,
+            method: 'post',
+            data: JSON.stringify(visionRequest),
+            contentType: "application/json"
+        })
+        .then(response => {
+            arrLabels = response['responses'][0]['labelAnnotations'];
+            arrLabels.forEach(label => {
+                var word = label.description;
+                // bubbleArray.push(label.description)
+                if (!(Object.keys(histogram).includes(word))) {
+                    histogram[word] = 1;
+                } 
+                else { 
+                    histogram[word] += 1 }
+                })
+                console.log(histogram)
+            })        
+    });
+    Promise.all(arrPhotoPromises)
+        .then(() => {
+            var arrHistogram = [];
+            Object.keys(histogram).forEach(key=>{
+                var miniObj = {};
+                miniObj['id'] = key;
+                miniObj['value'] = histogram[key];
+                arrHistogram.push(miniObj);
+            })
+            return arrHistogram;
+        })
+        .then(bubbleTwo)
+
+        
+
+});
+    
+
+
+
+// google language api
+
+var languageRequest = {
+    "document": {
+        "content": "THis is literally god's gift to earth. I love this API",
+        "type": "PLAIN_TEXT"
+        }
+    }
+
+$.ajax({
+    url: languageURL,
+    method: 'post',
+    data: JSON.stringify(languageRequest),
+    contentType: "application/json"
+  }).then(response=>{console.log(response)})
+
+
+//   0:
+//     formatted_phone_number: "(404) 996-2623"
+//     name: "Cooks & Soldiers"
+//     photos:(10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+//     place_id:"ChIJLTcZFewE9YgRHsW7wP1bEYs"
+//     price_level:3
+//     rating:4.6
+//     reviews: (5) [{…}, {…}, {…}, {…}, {…}]
+//     website:"http://cooksandsoldiers.com/"
